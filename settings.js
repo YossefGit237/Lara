2c697a3e3eb4dde5c4e7c64500b5906e2451a96a
YossefGit237 <bawusifufukar@gmail.com>
2025-01-15 19:17:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "3qp3zQZQ#4dDHvDA1zGKgjqDbI7JaGVeWmcUb1H6fg-bhpyqy91Q", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
